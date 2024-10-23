b11f29af67a3af5e58848ba2108dc39e2802a5c3
saikamara59 <mhsaidu@gmail.com>
2024-10-23 17:09:45 -0400

    updated fighters list and changed it to my own with images involved

--- src/App.jsx
+++ src/App.jsx
@@ -6,76 +6,76 @@ import React from "react";
 
 const App = () => {
 
-const [zombieFighters,setZombieFighters] = useState([
+const [dragonBallFighters,setDragonBallFighters] = useState([
   {
-    name: 'Survivor',
+    name: 'Goku',
     price: 12,
     strength: 6,
     agility: 4,
-    img: 'https://via.placeholder.com/150/92c952',
+    img: "https://dragonball-api.com/transformaciones/goku_ssj4.webp",
   },
   {
-    name: 'Scavenger',
+    name: 'Vegeta',
     price: 10,
     strength: 5,
     agility: 5,
-    img: 'https://via.placeholder.com/150/771796',
+    img: "https://dragonball-api.com/transformaciones/vegeta mega instinto.webp",
   },
   {
-    name: 'Shadow',
+    name: 'Frieza',
     price: 18,
     strength: 7,
     agility: 8,
-    img: 'https://via.placeholder.com/150/24f355',
+    img: "https://dragonball-api.com/transformaciones/Freezer_3.webp",
   },
   {
-    name: 'Tracker',
+    name: 'Piccolo',
     price: 14,
     strength: 7,
     agility: 6,
-    img: 'https://via.placeholder.com/150/d32776',
+    img: "https://dragonball-api.com/characters/picolo_normal.webp",
   },
   {
-    name: 'Sharpshooter',
+    name: 'Gohan',
     price: 20,
     strength: 6,
     agility: 8,
-    img: 'https://via.placeholder.com/150/1ee8a4',
+    img:"https://dragonball-api.com/characters/gohan.webp",
   },
   {
-    name: 'Medic',
+    name: 'Gotenks',
     price: 15,
     strength: 5,
     agility: 7,
-    img: 'https://via.placeholder.com/150/66b7d2',
+    img: "https://dragonball-api.com/characters/Gotenks_Artwork.webp",
   },
   {
-    name: 'Engineer',
+    name: 'Master Roshi',
     price: 16,
     strength: 6,
     agility: 5,
-    img: 'https://via.placeholder.com/150/56acb2',
+    img: "https://dragonball-api.com/characters/roshi.webp",
   },
   {
-    name: 'Brawler',
+    name: 'Android 17',
     price: 11,
     strength: 8,
     agility: 3,
-    img: 'https://via.placeholder.com/150/8985dc',
+    img: "https://dragonball-api.com/characters/17_Artwork.webp",
   },
   {
-    name: 'Infiltrator',
+    name: 'Majin Buu',
     price: 17,
     strength: 5,
     agility: 9,
-    img: 'https://via.placeholder.com/150/392537',
+    img: "https://dragonball-api.com/characters/BuuGordo_Universo7.webp",
   },
   {
-    name: 'Leader',
+    name: 'Jiren',
     price: 22,
     strength: 7,
     agility: 6,
-    img: 'https://via.placeholder.com/150/602b9e',
+    img: "https://dragonball-api.com/characters/Jiren.webp",
   },
 ]);
 
@@ -121,14 +121,14 @@ const handleRemoveFighter = (fighter) => {
 
   return (
     <div className="App">
-      <h1> Zombie Fighters </h1>
+      <h1> Dragon Ball Fighters </h1>
       <p><strong>Money:</strong> ${money}</p>
       <p><strong>Total Team Strength:</strong>{totalStrength}</p>
       <p><strong>Total Team Agility:</strong>{totalAglity}</p>
 
-      <h2>Avaliable Zombie Fighters</h2>
+      <h2>Avaliable Dragon Ball Fighters</h2>
       <ul className= "fighters-list">
-        {zombieFighters.map((fighter,index)=>(
+        {dragonBallFighters.map((fighter,index)=>(
           <li key = {index} className= "fighter-card">
             <img src={fighter.img} alt={`${fighter.name} image`} className="fighter-image"/>
             <h3>{fighter.name}</h3>
